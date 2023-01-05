0e169297deae225e30a211af8351be663153e5b0
TanakaMasunda <tankapmasunda@gmail.com>
2023-01-06 01:03:58 +0200

    Update main.ts

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,32 +1,37 @@
-var student1 = {
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+}
+
+const student1: Student = {
     firstName: 'Tana',
     lastName: 'Mbindi',
     age: 25,
-    location: 'Harare'
-};
-var student2 = {
+    location: 'harare',
+}
+
+const student2: Student = {
     firstName: 'Tate',
-    lastName: 'Mbindi',
+    lastName: 'husband',
     age: 32,
-    location: 'chiyadzwa'
-};
-var studentsList = [student1, student2];
-var table = document.createElement('table');
-var tbody = document.createElement('tbody');
-table.style.background = "lightgray";
-table.appendChild(tbody);
-studentsList.forEach(function (student) {
-    var row = document.createElement('tr');
-    var nameCell = document.createElement('td');
-    var locationCell = document.createElement('td');
-    nameCell.textContent = student.firstName;
-    locationCell.textContent = student.location;
-    nameCell.style.border = "2px solid white";
-    locationCell.style.border = "2px solid white";
-    nameCell.style.padding = "4px";
-    locationCell.style.padding = "4px";
-    row.appendChild(nameCell);
-    row.appendChild(locationCell);
-    tbody.appendChild(row);
-});
-document.body.appendChild(table);
+    location: 'chiyadzwa',
+}
+
+let studentsList: Student[] = [];
+
+studentsList.push(student1);
+studentsList.push(student2);
+
+let myVar = document.createElement('table');
+
+for (let i = 0; i < studentsList.length; i++) {
+    let row = myVar.insertRow();
+    let name = row.insertCell();
+    let location = row.insertCell();
+    name.innerHTML = studentsList[i].firstName;
+    location.innerHTML = studentsList[i].location;
+}
+
+document.body.appendChild(myVar);
