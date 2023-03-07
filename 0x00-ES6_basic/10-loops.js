893a0ee6e3035ed77c5186115387f9095ce2f273
TanakaMasunda <tankapmasunda@gmail.com>
2023-03-07 23:25:06 +0200

    Delete 0x00-ES6_basic directory

--- 0x00-ES6_basic/10-loops.js
+++
@@ -1,8 +0,0 @@
-export default function appendToEachArrayValue(array, appendString) {
-  for (var idx in array) {
-    var value = array[idx];
-    array[idx] = appendString + value;
-  }
-
-  return array;
-}
